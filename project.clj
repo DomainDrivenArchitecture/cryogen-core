fa3d903c01050d71c649f699144909c387af4999
jem <michael.jerger@meissa-gmbh.de>
2019-11-29 15:53:06 +0100

    update deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject dda/cryogen-core "0.1.54-SNAPSHOT"
+(defproject dda/cryogen-core "0.2.0-SNAPSHOT"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.10.0"]
-                           [camel-snake-kebab "0.4.0"]
-                           [cheshire "5.8.1"]
-                           [clj-rss "0.2.3"]
+                           [camel-snake-kebab "0.4.1"]
+                           [cheshire "5.9.0"]
+                           [clj-rss "0.2.5"]
                            [clj-text-decoration "0.0.3"]
                            [enlive "1.1.6"]
                            [hawk "0.2.11"]
@@ -14,6 +14,6 @@
                            [io.aviso/pretty "0.1.37"]
                            [me.raynes/fs "1.4.6"]
                            [pandect "0.6.1"]
-                           [selmer "1.12.12"]]
+                           [selmer "1.12.17"]]
             :deploy-repositories [["snapshots" :clojars]
                                   ["releases" :clojars]])
