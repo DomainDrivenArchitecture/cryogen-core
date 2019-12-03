90935bab43cd588b33700f3ec32716b9cc9cccb1
jem <michael.jerger@meissa-gmbh.de>
2019-12-03 17:07:42 +0100

    "Mob Session DONE [ci-skip]"

--- src/cryogen_core/classpath_able_io.clj
+++ src/cryogen_core/classpath_able_io.clj
@@ -3,5 +3,18 @@
             [clojure.string :as s]
             [me.raynes.fs :as fs]))
 
+(defn copy-resources
+  [source-path target-path]
+  (cond
+        (not (.exists (io/file source-path)))
+        (throw (IllegalArgumentException. (str "resource " source-path " not found")))
+        (.isDirectory (io/file source-path))
+        (copy-dir source-path target-path ignored-files)
+        :else
+        (fs/copy source-path target-path)))
+
 (defn copy-resources-from-theme
-  [theme target])
+  [theme target]
+  (let [source-path (str "templates/themes/" theme "/js")
+        target-path (str target "/js")]
+    copy-resources source-path target-path))
