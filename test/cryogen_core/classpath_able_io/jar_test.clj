505ab143a33dfc742bc7d01253d78f115cca4a99
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2020-02-04 19:21:14 +0100

    Created first draft for jar.clj and jar_test.clj

---
+++ test/cryogen_core/classpath_able_io/jar_test.clj
@@ -0,0 +1,20 @@
+;   Copyright (c) meissa. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns cryogen-core.classpath-able-io.jar-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [schema.core :as s]
+            [cryogen-core.file-test-tools :as ftt]
+            [cryogen-core.classpath-able-io.jar :as sut])
+  (:import [java.net URI]))
+
+(deftest test-is-from-classpath-jar?
+  (is
+   (sut/is-from-classpath-jar? (.toURI (io/resource "dummy"))))
+  )
